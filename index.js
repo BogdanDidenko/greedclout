4582d8fc63581d3f72d34aaa69129ebbdfd28db6
Bogdan Didenko <bogdan@webspellchecker.net>
2021-04-10 01:04:35 +0300

    fix bug add .htaccess

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ window.onload = function(){
   fetch('https://www.bitcloutpulse.com/').then(function(response, body){
     response.text().then((data)=>{
       var $ = document.getElementById.bind(document)
-      var x1 = 0, x2 = 0;
+      var x1 = 0, x2 = 0, y = 0;
       bitclout_price = get_token_price(data)
       $('price').innerHTML = '' + bitclout_price
       $('compute').onclick = function(){
-        let current_token_price = parseFloat($('current_token_price').value)
+        let current_token_price = parseFloat($('current_token_price').value.replace(',', '.'))
         if (current_token_price > 0) {
+          y = current_token_price
           let coins_supply = (10 * Math.sqrt(10/3) * Math.sqrt(y)) / (Math.sqrt(bitclout_price))
           x1 = (0.001 * coins_supply**3) * bitclout_price
         }
-        let y = parseFloat($('expected_token_price').value)
+        y = parseFloat($('expected_token_price').value.replace(',', '.'))
         let coins_supply = (10 * Math.sqrt(10/3) * Math.sqrt(y)) / (Math.sqrt(bitclout_price))
         x2 = (0.001 * coins_supply**3) * bitclout_price
+        if (x2 <= x1) {
+          alert('Expected token price should be larger than Current token price');
+          return
+        }
 
         $('out').value = '' + (x2 - x1).toFixed(2) //+ '$'
       }
