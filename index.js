3db25fd77f356fe4b790ee414f9d9156ec5f7660
Bogdan Didenko <bogdan@webspellchecker.net>
2021-04-18 23:25:32 +0300

    fix bitclout price autoupdate thx cloutcompare:)

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 window.onload = function(){
   function get_token_price(data){
-    substr = '$BitClout: ~$';
-    start_pos = data.indexOf(substr);
-    end_pos = data.slice(start_pos + substr.length).indexOf('USD');
-    bitclout_price = parseFloat(data.slice(start_pos + substr.length, start_pos + substr.length + end_pos));
-    if (isNaN(bitclout_price)) {
-      bitclout_price = 157.90
-    }
-    $('#price').html('' + bitclout_price);
+    data = JSON.parse(data)
+    bitclout_price = data.data.bitclout_price.bitclout_bitcoin_exchange_rate * data.data.bitclout_price.bitcoin_usd_exchange_rate
+    // substr = '$BitClout: ~$';
+    // start_pos = data.indexOf(substr);
+    // end_pos = data.slice(start_pos + substr.length).indexOf('USD');
+    // bitclout_price = parseFloat(data.slice(start_pos + substr.length, start_pos + substr.length + end_pos));
+    // if (isNaN(bitclout_price)) {
+    //   bitclout_price = 157.90
+    // }
+    $('#price').html('' + bitclout_price.toLocaleString());
     return bitclout_price;
   }
 
@@ -50,64 +52,40 @@ window.onload = function(){
       return [expected_usd_locked - usd_locked, null];
     }
   }
-  // fetch('https://www.bitcloutpulse.com/').then(function(response, body){
-    // response.text().then((data)=>{
-  data = ''
-  var bitclout_price = get_token_price(data)
-  // setInterval(()=>{
-  //   fetch('https://bitclout.com/', {
-  //     mode: 'cors',
-  //     headers: {
-  //       'Access-Control-Allow-Origin':'*'
-  //     }
-  //   }).then(function(response, body){
-  //     response.text().then((data)=>{
-  //       bitclout_price = get_token_price(data)
-  //     });
-  //   });
-  // }, 1000)
-  $('.compute').on('click', (e)=>{
-    e.stopPropagation();
-    e.preventDefault()
-    target = e.target;
-    action = target.dataset.attrValue;
-    // var x1 = 0, x2 = 0, y = 0;
-    var current_token_price = 0, coins_supply = 0, usd_locked = 0,
-      current_token_price = to_float($('#current_token_price').val() || '0');
-    if (current_token_price > 0) {
-      coins_supply = (10 * Math.sqrt(10/3) * Math.sqrt(current_token_price)) / (Math.sqrt(bitclout_price));
-      usd_locked = (0.001 * coins_supply**3) * bitclout_price;
-    }
-    input_value = to_float($('#'+ action +'_amount').val());
-    if (isNaN(input_value)) {return}
-    // debugger
-    res = compute_actions[action](current_token_price, coins_supply, usd_locked, input_value, bitclout_price)
-    let out_value = res[0], error = res[1];
-    if (error != null) {
-      alert(error)
-      return
-    }
-    $('#'+action+'_out').val(out_value.toLocaleString() + ' $')
-  })
-      // var $ = document.getElementById.bind(document)
-      // $('compute').onclick = function(){
-      //   var x1 = 0, x2 = 0, y = 0;
-      //   let current_token_price = to_float($('current_token_price').value)
-      //   if (current_token_price > 0) {
-      //     let coins_supply = (10 * Math.sqrt(10/3) * Math.sqrt(y)) / (Math.sqrt(bitclout_price))
-      //     x1 = (0.001 * coins_supply**3) * bitclout_price
-      //   }
-      //   y = to_float($('expected_token_price').value)
-      //   if (isNaN(y)) {return}
-      //   let coins_supply = (10 * Math.sqrt(10/3) * Math.sqrt(y)) / (Math.sqrt(bitclout_price))
-      //   x2 = (0.001 * coins_supply**3) * bitclout_price
-      //   if (x2 <= x1) {
-      //     alert('Expected token price should be larger than Current token price');
-      //     return
-      //   }
+  fetch('https://cloutcompare.com/api/bitclout/price').then(function(response, body){
+    response.text().then((data)=>{
+    var bitclout_price = get_token_price(data)
+    setInterval(()=>{
+      fetch('https://cloutcompare.com/api/bitclout/price').then(function(response, body){
+        response.text().then((data)=>{
+          bitclout_price = get_token_price(data)
+        });
+      });
+    }, 10000)
+    $('.compute').on('click', (e)=>{
+      e.stopPropagation();
+      e.preventDefault()
+      target = e.target;
+      action = target.dataset.attrValue;
+      // var x1 = 0, x2 = 0, y = 0;
+      var current_token_price = 0, coins_supply = 0, usd_locked = 0,
+        current_token_price = to_float($('#current_token_price').val() || '0');
+      if (current_token_price > 0) {
+        coins_supply = (10 * Math.sqrt(10/3) * Math.sqrt(current_token_price)) / (Math.sqrt(bitclout_price));
+        usd_locked = (0.001 * coins_supply**3) * bitclout_price;
+      }
+      input_value = to_float($('#'+ action +'_amount').val());
+      if (isNaN(input_value)) {return}
+      // debugger
+      res = compute_actions[action](current_token_price, coins_supply, usd_locked, input_value, bitclout_price)
+      let out_value = res[0], error = res[1];
+      if (error != null) {
+        alert(error)
+        return
+      }
+      $('#'+action+'_out').val(out_value.toLocaleString() + ' $')
+    })
 
-      //   $('out').value =  (x2 - x1).toLocaleString() //+ '$'
-      // }
-  // })
-  // })
+    })
+  })
 }
